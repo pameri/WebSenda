109d68bf1244c014ccf95482731de49d1681c3aa
percy ameri <percy.ameri@gmail.com>
2014-06-19 13:14:07 -0500

    actualizando models  de app main

--- websenda/apps/main/models.py
+++ websenda/apps/main/models.py
@@ -11,3 +11,16 @@ class Empresa(models.Model):
     my_foto = models.ImageField(upload_to=image_path)
     background = models.ImageField(upload_to=image_path)    
     status = models.BooleanField(default=False)
+
+
+class Cliente(models.Model):
+    
+    def image_path(self,filename):
+        ruta = "setting/%s/%s" % (self.setting_name,str(filename))
+        return ruta
+    
+    cliente = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=200)    
+    foto = models.ImageField(upload_to=image_path)
+    background = models.ImageField(upload_to=image_path)    
+    status = models.BooleanField(default=False)
